b965df3cd6d354b07b598942ef28ed90c1a385c8
parry Pan <a84155844@gmail.com>
2024-07-30 01:22:44 +0800

    #1 打底階段 1. 定時檢查登入狀態

--- src/routers/privateRoute.js
+++ src/routers/privateRoute.js
@@ -2,24 +2,32 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../store";
 import Index from "./index";
-import { useEffect } from "react";
+import { useEffect, useCallback, useRef } from "react";
 import api from "../common/api";
 
 function PrivateRoutes({ forceToLogin }) {
   const location = useLocation();
   const navigate = useNavigate();
+  const intervalID = useRef();
   const store = useStore();
+  const checkLogin = useCallback(async () => {
+    const res = await api.getVerify();
+    if (res.status) {
+      store.user.setLogin(true);
+      store.user.setInfo(res.data);
+    } else {
+      store.user.setLogin(false);
+      forceToLogin && navigate("/login");
+    }
+  }, []);
   useEffect(() => {
-    (async () => {
-      const res = await api.getVerify();
-      if (res.status) {
-        store.user.setLogin(true);
-        store.user.setInfo(res.data);
-      } else {
-        store.user.setLogin(false);
-        forceToLogin && navigate("/login");
-      }
-    })();
+    checkLogin();
+    intervalID.current = setInterval(() => {
+      checkLogin();
+    }, 60000);
+    return () => {
+      clearInterval(intervalID.current);
+    };
   }, [location]);
 
   return <Index />;
