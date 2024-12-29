164ddd8b5fce7571b34e6320fcf1646186f02d80
Parry Pan <a84155844@gmail.com>
2024-12-29 22:02:53 +0800

    #1 1. 個人資訊頁component 切分調整

--- src/routers/info/my.js
+++ src/routers/info/my.js
@@ -1,18 +1,23 @@
 import style from "./my.module.css";
+import { Box } from "@mui/material";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
-import Interest from "./interest";
-import Job from "./job";
 import Intro from "./intro";
 
 export default function My() {
   return (
     <div>
-      <Intro></Intro>
-      <Job></Job>
-      <Interest></Interest>
+      <Box
+        sx={{
+          width: "100%",
+          mb: 2,
+          borderRadius: 3,
+        }}
+      >
+        <Intro></Intro>
+      </Box>
     </div>
   );
 }
