904a0de97bc7f9bffc5a3d49e2c123f880b600ec
Parry Pan <a84155844@gmail.com>
2025-02-28 23:07:12 +0800

    1. 錯誤頁面調整

--- src/routers/error/error.js
+++ src/routers/error/error.js
@@ -1,3 +1,18 @@
+import image from "./../../public/error.svg";
+import { Link } from "react-router-dom";
+import style from "./error.module.css";
 export default function Error() {
-  return <div>error</div>;
+  return (
+    <div className={style.wrapper}>
+      <img src={image} className={style.img}></img>
+      <div className={style.warn}>
+        <div className={style.text}>
+          <div>發生錯誤</div>
+          <Link to="/" reloadDocument>
+            <button className={style.button}>回首頁</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
