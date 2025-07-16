3b51216a9f2470c508f9d9c0815f54ea31e12046
panchinpeng <a84155844@gmail.com>
2025-07-16 16:57:05 +0800

    1. SEO優化 2. 當好友貼文與推薦貼文都沒有時UI調整

--- src/component/recommendArticle/recommendArticle.js
+++ src/component/recommendArticle/recommendArticle.js
@@ -4,86 +4,95 @@ import PostArticle from "../postArticle/postArticle";
 import style from "./recommendArticle.module.css";
 import Masonry from "masonry-layout";
 import useIntersectionObserver from "./../../hooks/useIntersectionObserver.js";
-export default function RecommendArticle() {
-  const [articles, setArticles] = useState([]);
-  const [isEnd, setIsEnd] = useState(false);
-  const nowPage = useRef(1);
-  const totalPage = useRef(null);
-  const loadingNextPageDOM = useRef();
-  const maxArticleId = useRef(0);
-  const [masonry, setMasonry] = useState(null);
-  const waterFallDOM = useRef();
-  const { startObserve, isIntersecting } = useIntersectionObserver();
-  useEffect(() => {
-    if (articles.length) {
-      maxArticleId.current = articles[articles.length - 1].id;
-      setMasonry(
-        new Masonry(waterFallDOM.current, {
-          columnWidth: 300,
-          itemSelector: ".waterFallItem2",
-          gutter: 10,
-        })
-      );
-    }
-  }, [articles]);
-  useEffect(() => {
-    (async () => {
-      const res = await api.getRecommendArticle();
-      if (res.data.results.length > 0) {
-        totalPage.current = res.data.totalPage;
-        setArticles(res.data.results);
-        startObserve(loadingNextPageDOM.current);
-      }
-    })();
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      if (isIntersecting) {
-        if (totalPage.current <= nowPage.current) {
-          setIsEnd(true);
-          return;
+export default function RecommendArticle({ setRecommendSize }) {
+    const [articles, setArticles] = useState([]);
+    const [isEnd, setIsEnd] = useState(false);
+    const nowPage = useRef(1);
+    const totalPage = useRef(null);
+    const loadingNextPageDOM = useRef();
+    const maxArticleId = useRef(0);
+    const [masonry, setMasonry] = useState(null);
+    const waterFallDOM = useRef();
+    const { startObserve, isIntersecting } = useIntersectionObserver();
+    useEffect(() => {
+        if (articles.length) {
+            maxArticleId.current = articles[articles.length - 1].id;
+            setMasonry(
+                new Masonry(waterFallDOM.current, {
+                    columnWidth: 300,
+                    itemSelector: ".waterFallItem2",
+                    gutter: 10,
+                })
+            );
         }
-        nowPage.current = nowPage.current + 1;
-        const recommendRes = await api.getRecommendArticle(
-          maxArticleId.current
-        );
-        if (recommendRes.status) {
-          setArticles((articles) => [
-            ...articles,
-            ...recommendRes.data.results,
-          ]);
-        }
-      }
-    })();
-  }, [isIntersecting]);
+        setRecommendSize instanceof Function &&
+            setRecommendSize(articles.length);
+    }, [articles]);
+    useEffect(() => {
+        (async () => {
+            const res = await api.getRecommendArticle();
+            if (res.data.results.length > 0) {
+                totalPage.current = res.data.totalPage;
+                setArticles(res.data.results);
+                startObserve(loadingNextPageDOM.current);
+            }
+        })();
+    }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (masonry) {
-        masonry.layout();
-      }
-    }, 200);
-  }, [masonry]);
-  return (
-    <>
-      <div className={style.title}>熱門動態</div>
-      <div className={`waterFall ${style.waterFall}`} ref={waterFallDOM}>
-        {articles.map((article) => (
-          <div
-            key={article.id}
-            className={`waterFallItem2 ${style.waterFallItem}`}
-          >
-            <PostArticle
-              article={article}
-              renderFn={() => masonry && masonry.layout()}
-            ></PostArticle>
-          </div>
-        ))}
-      </div>
+    useEffect(() => {
+        (async () => {
+            if (isIntersecting) {
+                if (totalPage.current <= nowPage.current) {
+                    setIsEnd(true);
+                    return;
+                }
+                nowPage.current = nowPage.current + 1;
+                const recommendRes = await api.getRecommendArticle(
+                    maxArticleId.current
+                );
+                if (recommendRes.status) {
+                    setArticles((articles) => [
+                        ...articles,
+                        ...recommendRes.data.results,
+                    ]);
+                }
+            }
+        })();
+    }, [isIntersecting]);
 
-      {isEnd && <div className={style.end}>到底了QQ</div>}
-      <div ref={loadingNextPageDOM}></div>
-    </>
-  );
+    useEffect(() => {
+        setTimeout(() => {
+            if (masonry) {
+                masonry.layout();
+            }
+        }, 200);
+    }, [masonry]);
+    return (
+        <>
+            {articles.length > 0 && (
+                <>
+                    <div className={style.title}>熱門動態</div>
+                    <div
+                        className={`waterFall ${style.waterFall}`}
+                        ref={waterFallDOM}
+                    >
+                        {articles.map((article) => (
+                            <div
+                                key={article.id}
+                                className={`waterFallItem2 ${style.waterFallItem}`}
+                            >
+                                <PostArticle
+                                    article={article}
+                                    renderFn={() => masonry && masonry.layout()}
+                                ></PostArticle>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+
+            {isEnd && <div className={style.end}>到底了QQ</div>}
+            <div ref={loadingNextPageDOM}></div>
+        </>
+    );
 }
--- src/routers/portal/portal.js
+++ src/routers/portal/portal.js
@@ -10,102 +10,115 @@ import Masonry from "masonry-layout";
 import useIntersectionObserver from "./../../hooks/useIntersectionObserver.js";
 
 function Portal() {
-  const store = useStore();
-  const [articles, setArticles] = useState([]);
-  const [isEnd, setIsEnd] = useState(false);
-  const loadingNextPageDOM = useRef();
-  const nowPage = useRef(1);
-  const totalPage = useRef(null);
-  const maxArticleId = useRef(0);
-  const masonry = useRef();
+    const store = useStore();
+    const [articles, setArticles] = useState([]);
+    const [isEnd, setIsEnd] = useState(false);
+    const [recommendSize, setRecommendSize] = useState();
+    const loadingNextPageDOM = useRef();
+    const nowPage = useRef(1);
+    const totalPage = useRef(null);
+    const maxArticleId = useRef(0);
+    const masonry = useRef();
+    const recommendArticleSize = useRef();
 
-  const { startObserve, isIntersecting } = useIntersectionObserver();
+    const { startObserve, isIntersecting } = useIntersectionObserver();
 
-  useEffect(() => {
-    if (articles.length) {
-      maxArticleId.current = articles[articles.length - 1].id;
-      masonry.current = new Masonry(
-        document.querySelector(".waterFall", {
-          columnWidth: 300,
-          itemSelector: ".waterFallItem",
-          gutter: 10,
-        })
-      );
-    }
-  }, [articles]);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => {
-    (async () => {
-      store.trends.getAllFriendTrends();
-      const articlesRes = await api.getArticle();
-
-      if (articlesRes.data.results.length) {
-        totalPage.current = articlesRes.data.totalPage;
-        setArticles(articlesRes.data.results);
-        startObserve(loadingNextPageDOM.current);
-      } else {
-        setIsEnd(true);
-      }
-    })();
-    return () => {
-      masonry.current && masonry.current.destroy();
-    };
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      if (isIntersecting) {
-        if (totalPage.current <= nowPage.current) {
-          setIsEnd(true);
-          return;
+    useEffect(() => {
+        if (articles.length) {
+            maxArticleId.current = articles[articles.length - 1].id;
+            masonry.current = new Masonry(
+                document.querySelector(".waterFall", {
+                    columnWidth: 300,
+                    itemSelector: ".waterFallItem",
+                    gutter: 10,
+                })
+            );
         }
-        nowPage.current = nowPage.current + 1;
-        const articlesNextRes = await api.getArticle(
-          undefined,
-          maxArticleId.current
-        );
-        if (articlesNextRes.status) {
-          setArticles((articles) => [
-            ...articles,
-            ...articlesNextRes.data.results,
-          ]);
-        }
-      }
-    })();
-  }, [isIntersecting]);
+    }, [articles]);
 
-  return (
-    <Box
-      sx={{
-        width: "100%",
-        paddingTop: "16px",
-        "@media (min-width: 960px)": {
-          width: "960px",
-        },
-      }}
-    >
-      <div className={`waterFall ${style.waterFall}`}>
-        {articles.map((article) => (
-          <div
-            key={article.id}
-            className={`waterFallItem ${style.waterFallItem}`}
-          >
-            <PostArticle
-              article={article}
-              renderFn={() => masonry.current && masonry.current.layout()}
-            ></PostArticle>
-          </div>
-        ))}
-      </div>
-      {nowPage.current === totalPage.current && (
-        <div>
-          <div className={style.friendEnd}>已看完所有好友動態</div>
-        </div>
-      )}
-      {isEnd && <RecommendArticle></RecommendArticle>}
-      <div ref={loadingNextPageDOM}></div>
-    </Box>
-  );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        (async () => {
+            store.trends.getAllFriendTrends();
+            const articlesRes = await api.getArticle();
+
+            if (articlesRes.data.results.length) {
+                totalPage.current = articlesRes.data.totalPage;
+                setArticles(articlesRes.data.results);
+                startObserve(loadingNextPageDOM.current);
+            } else {
+                setIsEnd(true);
+            }
+        })();
+        return () => {
+            masonry.current && masonry.current.destroy();
+        };
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            if (isIntersecting) {
+                if (totalPage.current <= nowPage.current) {
+                    setIsEnd(true);
+                    return;
+                }
+                nowPage.current = nowPage.current + 1;
+                const articlesNextRes = await api.getArticle(
+                    undefined,
+                    maxArticleId.current
+                );
+                if (articlesNextRes.status) {
+                    setArticles((articles) => [
+                        ...articles,
+                        ...articlesNextRes.data.results,
+                    ]);
+                }
+            }
+        })();
+    }, [isIntersecting]);
+
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                paddingTop: "16px",
+                "@media (min-width: 960px)": {
+                    width: "960px",
+                },
+            }}
+        >
+            <div className={`waterFall ${style.waterFall}`}>
+                {articles.map((article) => (
+                    <div
+                        key={article.id}
+                        className={`waterFallItem ${style.waterFallItem}`}
+                    >
+                        <PostArticle
+                            article={article}
+                            renderFn={() =>
+                                masonry.current && masonry.current.layout()
+                            }
+                        ></PostArticle>
+                    </div>
+                ))}
+            </div>
+            {nowPage.current === totalPage.current && (
+                <div>
+                    <div className={style.friendEnd}>已看完所有好友動態</div>
+                </div>
+            )}
+            {isEnd && (
+                <RecommendArticle
+                    setRecommendSize={setRecommendSize}
+                ></RecommendArticle>
+            )}
+            {recommendSize === 0 && articles.length === 0 && (
+                <div className={style.friendEnd}>
+                    沒有任何動態可以顯示 QQ 先找一些朋友吧
+                </div>
+            )}
+            <div ref={loadingNextPageDOM}></div>
+        </Box>
+    );
 }
 export default observer(Portal);
